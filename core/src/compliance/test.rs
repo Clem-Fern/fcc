49d9fc6954461042f5ce2bd48a6c5d3e74357a4e
Clem Fern <clemfern456@gmail.com>
2025-03-19 22:00:11 +0100

    test: match type optional

--- core/src/compliance/test.rs
+++ core/src/compliance/test.rs
@@ -84,3 +84,21 @@ fn test_process_parent_compliance_check_absent_ignore_enum_variant() {
         }
     }
 }
+
+#[test]
+fn test_process_parent_compliance_check_optional() {
+    let policy = FlatConfigCompliance::from_str(include_str!(
+        "../../test/process_parent_compliance_check/5_p.txt"
+    ))
+    .unwrap();
+    let config = FlatConfig::from_str(include_str!(
+        "../../test/process_parent_compliance_check/5_c.txt"
+    ))
+    .unwrap();
+    let result = process_parent_compliance_check(&policy, &config);
+
+    assert_eq!(result.len(), 3);
+    for r in result {
+        assert!(matches!(r.result, Ok(_)))
+    }
+}
